a495d87c82e4af8c8dda5dbccfb73de5a964f464
luxbl <108549352+luxbl@users.noreply.github.com>
2024-08-16 14:30:53 -0400

    Add files via upload

---
+++ DryCatFood.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetStore
+{
+    public class DryCatFood
+    {
+        public double Weight {get; set;}
+    }
+}
